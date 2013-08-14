4d17478c1a9fe21a53024e001746dfdcab479a6b
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-08-14 14:46:59 +0200

    Update String.js

--- String.js
+++ String.js
@@ -3,3 +3,42 @@ function humanize(string) {
 	string = string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 	return string;
 }
+
+function randomString() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    var string_length = 32;
+    var randomstring = '';
+    for (var i=0; i<string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum,rnum+1);
+    }
+    return randomstring;
+}
+
+function ISODateString(d){
+
+    function pad(n){return n<10 ? '0'+n : n}
+    return d.getUTCFullYear()+'-'
+        + pad(d.getUTCMonth()+1)+'-'
+        + pad(d.getUTCDate())+'T'
+        + pad(d.getUTCHours())+':'
+        + pad(d.getUTCMinutes())+':'
+        + pad(d.getUTCSeconds())+'Z'
+}
+
+var generateUserToken = function( m_conn ) {
+
+    var S4 = function() {
+        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    token = S4()+S4()+S4()+S4()+S4()+S4();
+
+    m_conn.set( memcachePrefix + token, '1', memcache_memorize_time, function( err ) {
+        if (err) {
+            if (debug) console.log("MEMCACHE ERROR DURING SET");
+        }
+    });
+
+    return token;   
+}
+
